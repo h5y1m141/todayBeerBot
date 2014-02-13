7d707bf216e8968703975a28324b4b3efeabe43b
HiroshiOyamada <h5y1m141@gmail.com>
2014-02-14 08:04:21 +0900

    dictに含まれる情報のみRTする

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 (function() {
-  var conf, twit, twitter;
+  var TinySegmenter, conf, modulePath, path, retweet, segmenter, twit, twitter, _;
 
   twitter = require('ntwitter');
 
   conf = require('config');
+
+  _ = require('underscore');
+
+  path = require("path");
+
+  modulePath = path.resolve(__dirname, "lib/tiny_segmenter-0.2.js");
+
+  TinySegmenter = require(modulePath).TinySegmenter;
+
+  segmenter = new TinySegmenter();
 
   twit = new twitter({
     consumer_key: conf.consumer_key,
@@ -13,13 +23,30 @@
   });
 
   twit.verifyCredentials(function(err, data) {}).getHomeTimeline(function(err, data) {
-    var tweet, _i, _len;
+    var tweet, _i, _len, _results;
+    _results = [];
     for (_i = 0, _len = data.length; _i < _len; _i++) {
       tweet = data[_i];
       if (tweet.text !== "undefined") {
-        console.log("name:" + tweet.user.name + " tweet:" + tweet.text);
+        _results.push(retweet(tweet));
+      } else {
+        _results.push(void 0);
       }
     }
+    return _results;
   });
+
+  retweet = function(targetTweet) {
+    var dict, result, segs;
+    dict = ['ペールエール', 'IPA', 'COECO'];
+    segs = segmenter.segment(targetTweet.text);
+    result = _.intersection(segs, dict);
+    if (result.length !== 0) {
+      console.log(targetTweet.id_str);
+      return twit.verifyCredentials(function(err, data) {}).retweetStatus(targetTweet.id_str, function(err, data) {
+        return console.log(data);
+      });
+    }
+  };
 
 }).call(this);
--- coffee/app.coffee
+++ coffee/app.coffee
@@ -1,5 +1,14 @@
 twitter = require('ntwitter')
 conf = require('config')
+_ = require('underscore')
+
+# 形態素解析のために外部ライブラリ読み込む
+path = require("path")
+modulePath = path.resolve(__dirname, "lib/tiny_segmenter-0.2.js")
+TinySegmenter = require(modulePath).TinySegmenter
+
+segmenter = new TinySegmenter()
+
 twit = new twitter(
   consumer_key        : conf.consumer_key
   consumer_secret     : conf.consumer_secret
@@ -24,5 +33,23 @@ twit.verifyCredentials((err, data) ->
 ).getHomeTimeline (err, data) ->
   for tweet in data
     if tweet.text isnt "undefined"
-      console.log "name:#{tweet.user.name} tweet:#{tweet.text}"
-  return
+      retweet(tweet)
+      
+# dictに含まれる情報のみをRTする
+retweet = (targetTweet) ->
+  dict = [
+    'ペールエール',
+    'IPA',
+    'COECO'
+    ]      
+  segs = segmenter.segment(targetTweet.text)
+  result = _.intersection(segs,dict)
+  if result.length isnt 0
+    console.log targetTweet.id_str
+    twit.verifyCredentials((err, data) ->
+      # console.log data
+
+    ).retweetStatus targetTweet.id_str,(err, data) ->
+      console.log data
+    
+
