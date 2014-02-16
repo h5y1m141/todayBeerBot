9f47f06fed92c7c3b88d9389dc39b241621e7065
HiroshiOyamada <h5y1m141@gmail.com>
2014-02-17 08:14:39 +0900

    implement retweet method

--- coffee/lib/todayBeerBot.coffee
+++ coffee/lib/todayBeerBot.coffee
@@ -2,7 +2,7 @@ class todayBeerBot
   constructor: () ->
     twitter = require('ntwitter')
     conf = require('config')
-    @_ = require('underscore')
+    @_ = require('underscore')    
     @moment = require('moment')
     @twit = new twitter(
       consumer_key        : conf.consumer_key
@@ -27,6 +27,73 @@ class todayBeerBot
           
       return callback tweets
       
-  retweet:(targetTweet) ->
-    return true      
+  retweet:(id_str,callback) ->
+    @twit.verifyCredentials((err, data) ->
+      # console.log data
+      
+    ).retweetStatus id_str,(err, data) ->
+      console.log "err is #{err} and data is #{data}"        
+      if err
+        callback err
+      else
+        callback data
+    
+  _checkIfTweet:(targetTweet) ->
+    id_str = targetTweet.id_str    
+    tweetTime = @moment(targetTweet.created_at)
+    currentTime = @moment()
+    
+    if @_hasCraftBeerKeyword(targetTweet.text) is true and @_withinTheLimitsOfTheTime(tweetTime, currentTime) is true
+      return true
+    else
+      return false
+          
+  # 引数にとるキーワードの中に開栓情報らしきものが含まれてるかどうか
+  # 判定するメソッド          
+  _hasCraftBeerKeyword:(contents) ->
+    flg = null
+    dict = [
+      '本日',
+      '開栓',
+      '開栓情報',
+      '限定ビール',    
+      'ペールエール',
+      'IPA',
+      'エール',
+      '箕面ビール',
+      '箕面',
+      'COECO',
+      '湘南ビール',
+      '伊勢角屋麦酒'    
+      ]
+    path = require("path")
+    modulePath = path.resolve(__dirname, "tiny_segmenter-0.2.js")
+    TinySegmenter = require(modulePath).TinySegmenter
+    segmenter = new TinySegmenter()
+    
+    segs = segmenter.segment(contents)
+    result = @_.intersection(segs,dict)
+    if result.length isnt 0
+      flg = true
+    else
+      flg = false
+    return flg
+    
+  # 指定時間内かどうかを判定するメソッド
+  # １時間＝3600秒で、その範囲内にある場合に処理を行いたい
+  # ため基準となる数値を以下で設定
+
+  _withinTheLimitsOfTheTime:(target,flgTime,theLimitsOfTheTime = 3600) ->
+    timeA = @moment(target)
+    diffResult = @moment(flgTime).diff(timeA)/1000
+    console.log " diffResult is #{diffResult}"
+    if diffResult > 0 and diffResult < theLimitsOfTheTime  
+      return true
+    else
+      return false
+
+    
+    
+    
+    
 exports.todayBeerBot = todayBeerBot
--- spec/todayBeerBot_spec.js
+++ spec/todayBeerBot_spec.js
@@ -20,15 +20,46 @@
         return done();
       });
     }, 5000);
-    return it('should be retweet', function() {
-      var targetTweet;
-      targetTweet = {
-        text: "これはテストのための投稿です本日の開栓情報",
-        created_at: "Mon Aug 31 02:04:47 +0000 2009",
-        name: "publican@洗足",
-        id_str: "70277990"
-      };
-      return expect(this.bot.retweet(targetTweet)).toBe(true);
+    it('should be retweet', function(done) {
+      var id_str;
+      id_str = "435170834790236160";
+      return this.bot.retweet(id_str, function(data) {
+        expect(typeof data).toEqual("object");
+        return done();
+      });
+    }, 8000);
+    it('開栓情報らしきものがツイートに含まれている場合にはtrueを返す', function() {
+      var contents;
+      contents = "本日は樽が沢山開いてます♪  『ballast Point Victory at sea』 『アウグスビール アウグスホワイト』 『湘南ビール ベルジャンゴールド』 『TYハーバー バーレーワイン";
+      return expect(this.bot._hasCraftBeerKeyword(contents)).toBe(true);
+    });
+    it('ツイートにビールの種類が含まれている場合にはtrueを返す', function() {
+      var contents;
+      contents = "本日は12時から17時まで短縮営業いたします！お越しの際は足元お気をつけ下さい(>_<) ☆雑穀ヴァイツェン ☆茜レッドエール ☆おがわポーター 小川町駅前はまだ雪がいっぱいです";
+      return expect(this.bot._hasCraftBeerKeyword(contents)).toBe(true);
+    });
+    it('開栓情報でもビールの種類の情報も含まないケースはfalseを返す', function() {
+      var contents;
+      contents = "今日も仕込んでます";
+      return expect(this.bot._hasCraftBeerKeyword(contents)).toBe(false);
+    });
+    it('指定時間内にある情報なのでtrueが返る', function() {
+      var currentTime, target;
+      target = "Sat Feb 15 17:03:19 +0000 2014";
+      currentTime = "Sat Feb 15 17:05:19 +0000 2014";
+      return expect(this.bot._withinTheLimitsOfTheTime(target, currentTime)).toBe(true);
+    });
+    it('指定時間内にないためfalseが返る', function() {
+      var currentTime, target;
+      target = "Sat Feb 15 7:03:19 +0000 2014";
+      currentTime = "Sat Feb 15 17:05:19 +0000 2014";
+      return expect(this.bot._withinTheLimitsOfTheTime(target, currentTime)).toBe(false);
+    });
+    return it('currentTimeよりもターゲットとなる情報が先になるというのは未来を意図するためこのようなことはないはずだがこのケースでもfalseが返る', function() {
+      var currentTime, target;
+      target = "Sat Feb 15 22:03:19 +0000 2014";
+      currentTime = "Sat Feb 15 17:05:19 +0000 2014";
+      return expect(this.bot._withinTheLimitsOfTheTime(target, currentTime)).toBe(false);
     });
   });
 
