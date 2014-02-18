83205e01189b74723b27f4b1839de214c6450ea5
HiroshiOyamada <h5y1m141@gmail.com>
2014-02-19 08:08:08 +0900

    fake implment post tweet after parse rss feed

--- app.js
+++ app.js
@@ -1,61 +1,47 @@
 (function() {
-  var TinySegmenter, conf, modulePath, moment, params, path, retweet, segmenter, twit, twitter, _;
-
-  twitter = require('ntwitter');
-
-  conf = require('config');
-
-  _ = require('underscore');
-
-  moment = require('moment');
+  var bot, feed, feedList, modulePath, moment, path, todayBeerBot, _i, _len;
 
   path = require("path");
 
-  modulePath = path.resolve(__dirname, "lib/tiny_segmenter-0.2.js");
+  modulePath = path.resolve(__dirname, "lib/todayBeerBot.js");
 
-  TinySegmenter = require(modulePath).TinySegmenter;
+  todayBeerBot = require(modulePath).todayBeerBot;
 
-  segmenter = new TinySegmenter();
+  moment = require("moment");
 
-  twit = new twitter({
-    consumer_key: conf.consumer_key,
-    consumer_secret: conf.consumer_secret,
-    access_token_key: conf.access_token_key,
-    access_token_secret: conf.access_token_secret
-  });
+  bot = new todayBeerBot();
 
-  params = {
-    count: 200
-  };
+  feedList = bot.feedList;
 
-  twit.verifyCredentials(function(err, data) {}).getHomeTimeline(params, function(err, data) {
-    var tweet, _i, _len, _results;
-    _results = [];
-    for (_i = 0, _len = data.length; _i < _len; _i++) {
-      tweet = data[_i];
-      if (tweet.text !== "undefined") {
-        _results.push(retweet(tweet));
-      } else {
-        _results.push(void 0);
+  for (_i = 0, _len = feedList.length; _i < _len; _i++) {
+    feed = feedList[_i];
+    bot.parseFeed(feed.rss, function(items) {
+      var currentTime, feedType, flg, item, postData, text, _j, _len1, _results;
+      if (items.length !== 0) {
+        _results = [];
+        for (_j = 0, _len1 = items.length; _j < _len1; _j++) {
+          item = items[_j];
+          currentTime = moment();
+          flg = bot._withinTheLimitsOfTheTime(item.pubDate, currentTime, 900000);
+          if (flg === true) {
+            feedType = item.meta["#type"];
+            if (feedType === 'atom') {
+              text = bot._htmlToText(item["atom:content"]["#"]);
+            } else {
+              text = bot._htmlToText(item["rss:description"]["#"]);
+            }
+            postData = ("更新日" + (moment(item.pubDate).format("MM-DD")) + "の「" + item.meta.title + "」の情報： " + text).substring(0, 140);
+            console.log(postData);
+            _results.push(bot.tweet(postData, function(data) {
+              return console.log(data);
+            }));
+          } else {
+            _results.push(void 0);
+          }
+        }
+        return _results;
       }
-    }
-    return _results;
-  });
-
-  retweet = function(targetTweet) {
-    var currentTime, dict, diffResult, result, segs, tweetTime;
-    dict = ['本日', '開栓', '開栓情報', '限定ビール', 'ペールエール', 'IPA', 'エール', '箕面ビール', '箕面', 'COECO', '湘南ビール', '伊勢角屋麦酒'];
-    segs = segmenter.segment(targetTweet.text);
-    result = _.intersection(segs, dict);
-    tweetTime = moment(targetTweet.created_at);
-    currentTime = moment();
-    diffResult = currentTime.diff(tweetTime) / 1000;
-    if (result.length !== 0 && diffResult < 3600) {
-      console.log(targetTweet.user.name + targetTweet.id_str);
-      return twit.verifyCredentials(function(err, data) {}).retweetStatus(targetTweet.id_str, function(err, data) {
-        return console.log(data);
-      });
-    }
-  };
+    });
+  }
 
 }).call(this);
